36e51ad48f02420965b113dc1bb6bff9ab8fec68
Maximka2009 <75164903+Maximka2009@users.noreply.github.com>
2020-12-12 19:07:57 +0300

    Add files via upload

---
+++ ДЗ/app.js
@@ -0,0 +1,5 @@
+let hey = prompt("What is your name?", "I dont know my name-_-");
+let surehey = prompt("What is your surenmae?", "I dont know my surename-_-");
+let age = prompt("В каком году вы родились?");
+alert("Hello, " + hey  + surehey);
+alert(2020 - age)
